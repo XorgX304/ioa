0daf89901834ccda20170897a55ecbce43c3ae0b
akayn <30512781+akayn@users.noreply.github.com>
2019-02-28 17:41:20 -0800

    Create setup.sh

---
+++ wd/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+unzip automation.zip
+unzip wda.zip
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew uninstall --ignore-dependencies libimobiledevice
+brew uninstall --ignore-dependencies usbmuxd
+brew install --HEAD usbmuxd
+brew unlink usbmuxd & brew link usbmuxd
+brew install --HEAD libimobiledevice
+brew link --overwrite libimobiledevice
+brew install ideviceinstaller
+brew link --overwrite ideviceinstaller
+brew install ios-webkit-debug-proxy
+brew install node
+brew install ideviceinstaller
+npm install -g appium
+brew install carthage
+npm install -g ios-deploy
+npm install -g deviceconsole
+npm install iproxy
